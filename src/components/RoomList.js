21c81ffa36b9e5248ffd20e7b7d2b46501c701de
Tai Dang <tai.dangit@gmail.com>
2018-12-07 15:39:21 -0700

    Checkpoint 3 create chat rooms: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         rooms: []
+         rooms: [],
+         newChatRoom: ''
       };
       this.roomsRef = this.props.firebase.database().ref('rooms');
    }
@@ -18,13 +19,40 @@ class RoomList extends Component {
       });
     }
 
+    newChatNameChanged(event) {
+       this.setState({
+          newChatRoom: event.target.value
+       });
+      //  console.log(event.target.value);
+    }
+
+    createRoom (event){
+      event.preventDefault();
+      // console.log("form submitted: " + this.state.newChatRoom);
+
+      this.roomsRef.push({
+         name: this.state.newChatRoom
+       });
+
+       //reset form
+       document.getElementById("new-chat-room-form").reset();
+    }
+
     render() {
        return( 
           <div className="list-room-info">
             <h1>Available Chat Rooms:</h1>
              {this.state.rooms.map((room, index) => (
-                <div key={index}>{room.key} - {room.name}</div>
+                <div key={index}>{room.name}</div>
              ))}
+            
+            <form onSubmit={(event) => this.createRoom(event)} id="new-chat-room-form">
+               <label htmlFor="newChatRoom">Enter new name: </label>
+               <input onChange={(event) => this.newChatNameChanged(event)} id="newChatRoom" name="newChatRoom" />
+               <button type="submit">Create New Chat</button>
+            </form>
+
+         
           </div>
        );
     }
